45ec813b4ad931e316a82fc85588e99b133c49ff
BooopDev <73344221+BooopDev@users.noreply.github.com>
2022-12-29 00:11:53 +0000

    Rewrote a lot of the code and simplified it for additional expandability. Added some new features such config changes for items shown on a per tier basis as well as some additional checks and QOL improvements. Added Risk of Options as a dependancy for easier config changes during or before runs. Haven't run into any issues yet with invisible items or blank squares.

--- BetterCommandArtifact/BetterCommandArtifact.cs
+++ BetterCommandArtifact/BetterCommandArtifact.cs
@@ -1,93 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using BepInEx;
-using BepInEx.Configuration;
+﻿using BepInEx;
 using RoR2;
+using System.Linq;
+using System.Collections.Generic;
+using Option = RoR2.PickupPickerController.Option;
 using UnityEngine.Networking;
-using PickupIndex = RoR2.PickupIndex;
-using PickupTransmutationManager = RoR2.PickupTransmutationManager;
 
 namespace BetterCommandArtifact
 {
-    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-
+    [BepInPlugin(Guid, ModName, Version), BepInDependency("com.rune580.riskofoptions")]
     public class BetterCommandArtifact : BaseUnityPlugin
     {
-        public const string PluginGUID = PluginAuthor + "." + PluginName;
-        public const string PluginAuthor = "Boooooop";
-        public const string PluginName = "BetterCommandArtifact";
-        public const string PluginVersion = "1.2.0";
-
-        public static ConfigFile configFile = new(Paths.ConfigPath + "\\BetterCommandArtifact.cfg", true);
-
-        public static ConfigEntry<int> ItemAmount { get; set; }
-
+        const string
+            ModName = "Better Command Artifact",
+            Author = "Boooooop",
+            Guid = "com." + Author + "." + "BetterCommandArtifact",
+            Version = "1.2.0";
+        
         public void OnEnable()
         {
-            ItemAmount = configFile.Bind("BetterCommandArtifact", "itemAmount", 5, new ConfigDescription("Sets the total amount of items shown when opening a command artifact drop. \n Value must be Greater Than 0."));
-            Config.SettingChanged += ConfigOnSettingChanged;
-            On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact += SetOptions;
+            On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact += OnSetOptionsCommand;
+            On.RoR2.Artifacts.CommandArtifactManager.OnDropletHitGroundServer += OnDropletHitGround;
+            RoR2Application.onLoad += OnLoad;
         }
-        void ConfigOnSettingChanged(object sender, SettingChangedEventArgs e)
+
+        void OnLoad()
         {
-            if (ItemAmount.Value <= 0)
-                ItemAmount.Value = 1;
+            Settings.InitConfig(Config);
         }
+
         public void OnDisable()
         {
-            On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact -= SetOptions;
+            On.RoR2.PickupPickerController.SetOptionsFromPickupForCommandArtifact -= OnSetOptionsCommand;
+            On.RoR2.Artifacts.CommandArtifactManager.OnDropletHitGroundServer -= OnDropletHitGround;
         }
 
         [Server]
-        void SetOptions(On.RoR2.PickupPickerController.orig_SetOptionsFromPickupForCommandArtifact orig, RoR2.PickupPickerController self, PickupIndex pickupIndex)
+        void OnSetOptionsCommand(On.RoR2.PickupPickerController.orig_SetOptionsFromPickupForCommandArtifact orig, PickupPickerController self, PickupIndex pickupIndex)
         {
             if (!NetworkServer.active) return;
 
-            PickupIndex[] newSelection = PickupTransmutationManager.GetGroupFromPickupIndex(pickupIndex);
-            PickupPickerController.Option[] array;
-            if (newSelection == null)
+            self.SetOptionsServer(GetOptions(pickupIndex));
+        }
+
+        Option[] GetOptions(PickupIndex pickupIndex)
+        {
+            PickupIndex[] pickupGroup = PickupTransmutationManager.GetGroupFromPickupIndex(pickupIndex);
+
+            if (pickupGroup == null) return new Option[1] { new Option { available = true, pickupIndex = pickupIndex } };
+
+            List<Option> options = new();
+
+            for (int i = 0; i < pickupGroup.Length; i++)
             {
-                array = new PickupPickerController.Option[1]
-                {
-                    new PickupPickerController.Option
-                    {
-                        available = true,
-                        pickupIndex = pickupIndex
-                    }
-                };
+                PickupIndex pickupFromGroup = pickupGroup[i];
+                bool pickupAvailable = Settings.IncludeLockedItems.Value == 2 ? true : Run.instance.IsPickupAvailable(pickupFromGroup);
+                options.Add(new Option { available = pickupAvailable, pickupIndex = pickupFromGroup });
             }
+
+            var orderedOptions = from x in options orderby Run.instance.treasureRng.nextUlong select x;
+
+            PickupDef pickupDef = PickupCatalog.GetPickupDef(pickupIndex);
+            ItemTier pickupTier = pickupDef.itemTier;
+
+            bool isEquipment = pickupDef.equipmentIndex != EquipmentIndex.None;
+            int itemAmount = GetTierItemAmount(pickupTier, isEquipment, pickupDef.isLunar);
+
+            if (Settings.IncludeLockedItems.Value == 1) return orderedOptions
+                    .Where(x => x.available)
+                    .Take(itemAmount)
+                    .ToArray();
+
+            return orderedOptions
+                .Take(itemAmount)
+                .ToArray();
+        }
+
+        private void OnDropletHitGround(On.RoR2.Artifacts.CommandArtifactManager.orig_OnDropletHitGroundServer orig, ref GenericPickupController.CreatePickupInfo createPickupInfo, ref bool shouldSpawn)
+        {
+            PickupIndex pickupIndex = createPickupInfo.pickupIndex;
+            PickupDef pickupDef = PickupCatalog.GetPickupDef(pickupIndex);
+            ItemTier pickupTier = pickupDef.itemTier;
+
+            bool isEquipment = pickupDef.equipmentIndex != EquipmentIndex.None;
+            bool isBossItem = pickupTier is ItemTier.Boss or ItemTier.VoidBoss;
+
+            int itemAmount = GetTierItemAmount(pickupTier, isEquipment, pickupDef.isLunar);
+
+            PickupIndex[] pickupGroup = PickupTransmutationManager.GetGroupFromPickupIndex(pickupIndex);
+
+            if ((isBossItem && !Settings.MultipleBossItems.Value) || (isBossItem && pickupGroup == null) || itemAmount == 1)
+                shouldSpawn = true;
             else
+                orig(ref createPickupInfo, ref shouldSpawn);
+        }
+
+        int GetTierItemAmount(ItemTier itemTier, bool isEquipment, bool isLunar)
+        {
+            if (!Settings.UseItemTiers.Value) return Settings.DefaultItemsShown.Value;
+
+            if (isEquipment) return isLunar ? Settings.LunarEquipmentItemsShown.Value : Settings.EquipmentItemsShown.Value;
+
+            return itemTier switch
             {
-                Random rnd = new();
-                List<PickupIndex> list = new();
-
-                int extraItems = ItemAmount.Value;
-
-                if (pickupIndex != PickupIndex.none)
-                {
-                    list.Add(pickupIndex);
-                    extraItems--;
-                }
-
-                if (extraItems > 0)
-                {
-                    List<PickupIndex> additionalOptions = (from x in newSelection.ToList() orderby rnd.Next() select x).Where(x => (Run.instance.IsPickupAvailable(x) && x != pickupIndex)).Take(extraItems).ToList();
-                    list.AddRange(additionalOptions);
-                }
-
-                array = new PickupPickerController.Option[list.Count];
-                for (int i = 0; i < list.Count; i++)
-                {
-                    PickupIndex pickupIndex2 = list[i];
-                    array[i] = new PickupPickerController.Option
-                    {
-                        available = Run.instance.IsPickupAvailable(pickupIndex2),
-                        pickupIndex = pickupIndex2
-                    };
-                }
-            }
-            self.SetOptionsServer(array);
+                ItemTier.Tier1 => Settings.CommonItemsShown.Value,
+                ItemTier.Tier2 => Settings.UncommonItemsShown.Value,
+                ItemTier.Tier3 => Settings.LegendaryItemsShown.Value,
+                ItemTier.Lunar => Settings.LunarItemsShown.Value,
+                ItemTier.Boss => Settings.BossItemsShown.Value,
+                ItemTier.VoidTier1 => Settings.CommonVoidItemsShown.Value,
+                ItemTier.VoidTier2 => Settings.UncommonVoidItemsShown.Value,
+                ItemTier.VoidTier3 => Settings.LegendaryVoidItemsShown.Value,
+                ItemTier.VoidBoss => 1,
+                _ => 1,
+            };
         }
     }
 }
---
+++ BetterCommandArtifact/Settings.cs
@@ -0,0 +1,144 @@
+﻿using BepInEx.Configuration;
+using RiskOfOptions;
+using RiskOfOptions.OptionConfigs;
+using RiskOfOptions.Options;
+using RoR2;
+using RoR2.ContentManagement;
+using System;
+using System.Linq;
+
+namespace BetterCommandArtifact
+{
+    public static class Settings
+    {
+        #region Main Settings
+        public static ConfigEntry<bool> UseItemTiers { get; set; }
+        public static ConfigDefinition UseItemTiersDef = new("Settings", "Use Item Tiers?");
+        public static ConfigDescription UseItemTiersDesc = new("Control the amount of items shown in the Command Menu for each Tier \n If False, 'Default Items Shown' will be used for every Tier");
+
+        public static ConfigEntry<bool> MultipleBossItems { get; set; }
+        public static ConfigDefinition AllowMultipleBossDef = new("Settings", "Multiple Boss Items?");
+        public static ConfigDescription AllowMultipleBossDesc = new("Changes whether boss items drop as Command Cubes or drop a single item");
+
+        public static ConfigEntry<int> IncludeLockedItems { get; set; }
+        public static ConfigDefinition IncludeLockedItemsDef = new("Settings", "Include Locked Items?");
+        public static ConfigDescription IncludeLockedItemsDesc = new("Include locked items in the Command Menu\n(Locked due to Challenge Progression or DLC)\n" +
+                                                                             "0 = Show Locked Items\n1 = Hide Locked Items\n2 = Unlock All Items");
+        #endregion
+
+        #region Tier Settings
+        public static ConfigEntry<int> DefaultItemsShown { get; set; }
+        public static ConfigDefinition DefaultItemsShownDef = new ("Settings", "Default");
+        public static ConfigDescription DefaultItemsShownDesc = new ("Default amount of items shown in the Command Menu");
+               
+        public static ConfigEntry<int> CommonItemsShown { get; set; }
+        public static ConfigDefinition CommonItemsShownDef = new ("Settings", "Common");
+        public static ConfigDescription CommonItemsShownDesc = new ("Amount of Common items shown in the Command Menu");
+               
+        public static ConfigEntry<int> UncommonItemsShown { get; set; }
+        public static ConfigDefinition UncommonItemsShownDef = new ("Settings", "Uncommon");
+        public static ConfigDescription UncommonItemsShownDesc = new ("Amount of Uncommon items shown in the Command Menu");
+               
+        public static ConfigEntry<int> LegendaryItemsShown { get; set; }
+        public static ConfigDefinition LegendaryItemsShownDef = new ("Settings", "Legendary");
+        public static ConfigDescription LegendaryItemsShownDesc = new ("Amount of Legendary items shown in the Command Menu");
+               
+        public static ConfigEntry<int> CommonVoidItemsShown { get; set; }
+        public static ConfigDefinition CommonVoidItemsShownDef = new("Settings", "Common Void");
+        public static ConfigDescription CommonVoidItemsShownDesc = new ("Amount of Common Void items shown in the Command Menu");
+
+        public static ConfigEntry<int> UncommonVoidItemsShown { get; set; }
+        public static ConfigDefinition UncommonVoidItemsShownDef = new("Settings", "Uncommon Void");
+        public static ConfigDescription UncommonVoidItemsShownDesc = new("Amount of Uncommon Void items shown in the Command Menu");
+
+        public static ConfigEntry<int> LegendaryVoidItemsShown { get; set; }
+        public static ConfigDefinition LegendaryVoidItemsShownDef = new("Settings", "Legendary Void");
+        public static ConfigDescription LegendaryVoidItemsShownDesc = new("Amount of Legendary Voiditems shown in the Command Menu");
+
+        public static ConfigEntry<int> EquipmentItemsShown { get; set; }
+        public static ConfigDefinition EquipmentItemsShownDef = new ("Settings", "Equipment");
+        public static ConfigDescription EquipmentItemsShownDesc = new ("Amount of Equipment items shown in the Command Menu");
+
+        public static ConfigEntry<int> BossItemsShown { get; set; }
+        public static ConfigDefinition BossItemsShownDef = new("Settings", "Boss");
+        public static ConfigDescription BossItemsShownDesc = new("Amount of Boss items shown in the Command Menu");
+
+        public static ConfigEntry<int> LunarItemsShown { get; set; }
+        public static ConfigDefinition LunarItemsShownDef = new("Settings", "Lunar");
+        public static ConfigDescription LunarItemsShownDesc = new("Amount of Lunar items shown in the Command Menu");
+
+        public static ConfigEntry<int> LunarEquipmentItemsShown { get; set; }
+        public static ConfigDefinition LunarEquipmentItemsShownDef = new("Settings", "Lunar Equipment");
+        public static ConfigDescription LunarEquipmentItemsShownDesc = new("Amount of Lunar Equipment items shown in the Command Menu");
+
+        static IntSliderConfig itemsShownConfig = new() 
+        { 
+            min = 1,
+            max = 100,
+            checkIfDisabled = CheckItemTiers,
+        };
+        #endregion
+
+
+        public static void InitConfig(ConfigFile config)
+        {
+            var items = ContentManager.itemDefs;
+            var equipment = ContentManager.equipmentDefs;
+            int commonMax = items.Where(x => x.tier == ItemTier.Tier1).Count();
+            int uncommonMax = items.Where(x => x.tier == ItemTier.Tier2).Count();
+            int legendaryMax = items.Where(x => x.tier == ItemTier.Tier3).Count();
+            int commonVoidMax = items.Where(x => x.tier == ItemTier.VoidTier1).Count();
+            int uncommonVoidMax = items.Where(x => x.tier == ItemTier.VoidTier2).Count();
+            int legendaryVoidMax = items.Where(x => x.tier == ItemTier.VoidTier3).Count();
+            int equipmentMax = equipment.Where(x => !x.isLunar).Count();
+            int bossMax = items.Where(x => x.tier == ItemTier.Boss).Count();
+            int lunarMax = items.Where(x => x.tier == ItemTier.Lunar).Count();
+            int lunarEquipmentMax = equipment.Where(x => x.isLunar).Count();
+
+            MultipleBossItems = config.Bind(AllowMultipleBossDef, true, AllowMultipleBossDesc);
+            ModSettingsManager.AddOption(new CheckBoxOption(MultipleBossItems));
+
+            IncludeLockedItems = config.Bind(IncludeLockedItemsDef, 1, IncludeLockedItemsDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(IncludeLockedItems, new IntSliderConfig() { min = 0, max = 2 }));
+
+            UseItemTiers = config.Bind(UseItemTiersDef, true, UseItemTiersDesc);
+            ModSettingsManager.AddOption(new CheckBoxOption(UseItemTiers));
+
+            DefaultItemsShown = config.Bind(DefaultItemsShownDef, 5, DefaultItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(DefaultItemsShown, new IntSliderConfig { min = 1, max = 100, checkIfDisabled = CheckItemTiers }));
+
+            CommonItemsShown = config.Bind(CommonItemsShownDef, 5, CommonItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(CommonItemsShown, new IntSliderConfig { min = 1, max = commonMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            UncommonItemsShown = config.Bind(UncommonItemsShownDef, 5, UncommonItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(UncommonItemsShown, new IntSliderConfig { min = 1, max = uncommonMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            LegendaryItemsShown = config.Bind(LegendaryItemsShownDef, 5, LegendaryItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(LegendaryItemsShown, new IntSliderConfig { min = 1, max = legendaryMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            CommonVoidItemsShown = config.Bind(CommonVoidItemsShownDef, 3, CommonVoidItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(CommonVoidItemsShown, new IntSliderConfig { min = 1, max = commonVoidMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            UncommonVoidItemsShown = config.Bind(UncommonVoidItemsShownDef, 3, UncommonVoidItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(UncommonVoidItemsShown, new IntSliderConfig { min = 1, max = uncommonVoidMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            LegendaryVoidItemsShown = config.Bind(LegendaryVoidItemsShownDef, 1, LegendaryVoidItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(LegendaryVoidItemsShown, new IntSliderConfig { min = 1, max = legendaryVoidMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            EquipmentItemsShown = config.Bind(EquipmentItemsShownDef, 6, EquipmentItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(EquipmentItemsShown, new IntSliderConfig { min = 1, max = equipmentMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            BossItemsShown = config.Bind(BossItemsShownDef, 5, BossItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(BossItemsShown, new IntSliderConfig { min = 1, max = bossMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            LunarItemsShown = config.Bind(LunarItemsShownDef, 5, LunarItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(LunarItemsShown, new IntSliderConfig { min = 1, max = lunarMax, checkIfDisabled = CheckItemTiersInverted }));
+
+            LunarEquipmentItemsShown = config.Bind(LunarEquipmentItemsShownDef, 2, LunarEquipmentItemsShownDesc);
+            ModSettingsManager.AddOption(new IntSliderOption(LunarEquipmentItemsShown, new IntSliderConfig { min = 1, max = lunarEquipmentMax, checkIfDisabled = CheckItemTiersInverted }));
+        }
+
+        static bool CheckItemTiers() { return UseItemTiers.Value; }
+        static bool CheckItemTiersInverted() { return !UseItemTiers.Value; }
+    }
+}
